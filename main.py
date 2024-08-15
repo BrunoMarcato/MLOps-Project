d348efc91b697930a61e8628578f843a563fb820
Bruno Marcato <bruno11prog@outlook.com>
2024-08-15 11:28:04 -0300

    data ingestion step

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from mlproject import logger
+from mlproject.pipeline.data_ingestion_stage import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion"
+
+try:
+    logger.info(f"==== stage {STAGE_NAME} started =====")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f"==== stage {STAGE_NAME} completed ====\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
